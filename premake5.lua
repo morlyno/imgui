bccfb7e9c8aa289ffa332c8085c47b4ac40bf204
moro <moritz-eibl@web.de>
2025-08-13 22:37:53 +0200

    premake + gitignore

---
+++ premake5.lua
@@ -0,0 +1,28 @@
+project "ImGui"
+    kind "StaticLib"
+	language "C++"
+	cppdialect "C++20"
+	
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files {
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+        "imgui_tables.cpp",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp",
+
+		"misc/cpp/imgui_stdlib.h",
+		"misc/cpp/imgui_stdlib.cpp",
+	}
+
+    includedirs {
+        "./"
+    }
